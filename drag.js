485d89920b52d52a445821d137452c73cd978b80
Dev Gangwar <gangwardev50@gmail.com>
2022-08-13 10:37:47 +0530

    drag and drop functionality

---
+++ drag.js
@@ -0,0 +1,42 @@
+console.log("this is drag and top utility");
+
+const imgBox = document.querySelector('.img-box');
+const whiteBoxes = document.getElementsByClassName('whiteBox');
+
+
+// Events Listener for draggable img box
+imgBox.addEventListener('dragstart', (e)=>{
+    console.log("Drag Start has been triggered");
+   e.target.className += ' hold';
+   setTimeout(()=>{
+
+       e.target.className = ' hide'
+   }, 0)
+});
+
+imgBox.addEventListener('dragend', (e)=>{
+    console.log("Drag end has been triggred")
+    e.target.className = 'img-box';
+});
+
+for (whiteBox of whiteBoxes){
+    whiteBox.addEventListener('dragover', (e)=>{
+        e.preventDefault();
+        console.log("dragover method is triggered")
+
+    });
+    whiteBox.addEventListener('dragenter', (e)=>{
+        console.log("dragenter method is triggered")
+    
+    });
+    whiteBox.addEventListener('dragleave', ()=>{
+        console.log("dragleave method is triggered")
+
+
+    });
+    whiteBox.addEventListener('drop', (e)=>{
+        console.log("drop method is triggered")
+        e.target.append(imgBox);
+
+    });
+}
